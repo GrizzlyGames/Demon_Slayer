42eb99790a06ee86e13112f1bfb3822ed5c98449
GrizzlyGames <info@grizzlygames.co>
2017-01-12 17:32:58 -0500

    New Assets

---
+++ Demon_Slayer/Assets/My_Asserts/My_Scripts/Shoot_Projectile_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Projectile_Script : MonoBehaviour {
+
+    public GameObject projectile;           // Get projectile to be fired
+
+    [SerializeField]
+    private Transform projectileSpawnPoint;
+
+    private Projectile_Script projectileScript;
+
+    public void ShootPrimaryProjectile()           // Public funciton called by the player
+    {
+        Debug.Log("Shoot Projectile Called");
+
+        GameObject go = Instantiate(projectile, projectileSpawnPoint.position, Quaternion.identity);
+        projectileScript = go.GetComponent(typeof(Projectile_Script)) as Projectile_Script;
+
+        projectileScript.ShootPrimaryProjectile();         // Call the function ShootPrjectile attached to the Projectile Gameobject
+    }
+}
---
+++ Demon_Slayer/Assets/Projectile_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Script : MonoBehaviour {
+
+    public float initialForce;              // Initial force when projectile is fired
+    public float reduceForceAmount;         // The amount of force to reduce over time
+    public float reduceForceTime;           // The rate inwhich the force is reduced over time
+
+    [SerializeField]
+    private Rigidbody rb;                   // Rigidbody to be used to apply force exposed tp projectile
+
+    public void ShootPrimaryProjectile()           // Public funciton called by the player
+    {
+        InitialForce();
+        Debug.Log("Shoot Projectile Function Called");
+    }
+
+    void InitialForce()
+    {
+        
+        rb.AddForce(new Vector3(0,0,transform.localPosition.z) * initialForce);
+    }
+
+    void ReduceForce()
+    {
+
+    }
+
+    IEnumerator ReduceForceDelay()
+    {
+        yield return new WaitForSeconds(reduceForceTime);           // The rate inwhich the force will be reduced
+        ReduceForce();          // Call the fuction to reduce the force
+    }
+}
